4494813b54b9eed165c654b1cea4625eacc5bc81
Logan Gallaway <lwgallaway@users.noreply.github.com>
2017-04-23 09:17:42 -0400

    process scrap dataframe
    process the scrapped data.frame into something that the efficiencyGap function can use.

---
+++ EfficiencyScrapTransform.R
@@ -0,0 +1,8 @@
+EfficiencyScrapTransform<-fucntion(ScrapDF){
+  #This script will be used to rearrange the data into a format that is workable with
+  #the efficiency gap function. It will also be removing all unconstested districts from
+  #data. I realize that this is not the most accurate way to measure a state, but as of
+  #this time I have not determined a good way to repersent those districts. 
+  
+  
+}
