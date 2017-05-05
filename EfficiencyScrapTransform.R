5db22759500528ad1f03dbc0d44a7557b7119d8d
Logan Gallaway <lwgallaway@gmail.com>
2017-05-04 20:38:30 -0400

    dev
    developing the data transform script

--- EfficiencyScrapTransform.R
+++ EfficiencyScrapTransform.R
@@ -1,4 +1,4 @@
-EfficiencyScrapTransform<-fucntion(scrapDF){
+EfficiencyScrapTransform<-function(scrapDF){
   #This script will be used to rearrange the data into a format that is workable with
   #the efficiency gap function. It will also be removing all unconstested districts from
   #data. I realize that this is not the most accurate way to measure a state, but as of
@@ -10,8 +10,16 @@ EfficiencyScrapTransform<-fucntion(scrapDF){
   EF_frame = data.frame(District = 0, R_votes = 0, D_votes = 0, I_votes = 0)
   
   for(i in state){
-    subset(scrapDF, scrapDF[,5] == i)
-    
+    temp<-subset(scrapDF, scrapDF[,5] == i)
+    for (j in 1:len(temp)){
+      if(is.na(temp[j,3]) == TRUE){
+        next
+      }
+      if(substr(temp[j,1]) == "D"){  #assigns to correct district by match between the 2 DF's
+        EF_frame[temp[j,4],3]<-temp[j,3]
+        EF_frame
+      }
+    }
   }
   
 }
